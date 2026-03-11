70eae6c34b0741b6ff86bc1a7518c6a1d5b84c6b
michaelsainttt <michaelsaint2005@gmail.com>
2026-03-10 20:43:33 -0400

    changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from routes.books_routes import books_bp
 from routes.wishlist_routes import wishlist_bp
+from routes.user_routes import user_bp
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ def health():
 
 app.register_blueprint(books_bp)
 app.register_blueprint(wishlist_bp)
+app.register_blueprint(user_bp)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5001))
